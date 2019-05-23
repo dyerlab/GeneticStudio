1038b7f3da863bd36588b0455739b02f69beedbc
dyerlab <rjdyer@vcu.edu>
2019-05-23 16:00:35 -0400

    playing with git hooks

--- sourcekitty.sh
+++ sourcekitty.sh
@@ -18,7 +18,6 @@ perl -0pe 's/\]\n\[/,/g' sk.output  > geneticstudio.output
 jazzy --sourcekitten-sourcefile geneticstudio.output
 open docs/index.html
 
-
 #sourcekitten doc -- -workspace GeneticStudio.xcworkspace -scheme matrix > sk.matrix.output
 #sourcekitten doc -- -workspace GeneticStudio.xcworkspace -scheme genetic > sk.genetic.output
 #cat sk.matrix.output > sk.output
