e37b23d0770dc50f2632ca8d01ca11c215f6d252
dyerlab <rjdyer@vcu.edu>
2019-05-30 15:32:29 -0400

    adding sideview

---
+++ Population Graphs/Population Graphs/Document/OutlineViewController.swift
@@ -0,0 +1,18 @@
+//
+//  OutlineViewController.swift
+//  Population Graphs
+//
+//  Created by Rodney Dyer on 5/30/19.
+//  Copyright © 2019 Rodney Dyer. All rights reserved.
+//
+
+import Cocoa
+
+class OutlineViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
