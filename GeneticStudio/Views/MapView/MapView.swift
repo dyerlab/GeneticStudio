5e5d9c6bb7a0db223d491eb77d70dd56e1da6ca9
Rodney <rjdyer@vcu.edu>
2023-06-25 10:59:20 -0400

    working on map stuff

--- GeneticStudio/Views/MapView/MapView.swift
+++ GeneticStudio/Views/MapView/MapView.swift
@@ -7,15 +7,25 @@
 
 import MapKit
 import SwiftUI
+import DLGenetic
 
 struct MapView: View {
-   
+    @Binding var locations: [Location]
     
     var body: some View {
-        Map()
+        Map {
+            ForEach(locations) { location in
+                Marker( location.name,
+                        systemImage: "tree",
+                        coordinate: location.coordinate)
+            }
+        }
     }
 }
 
 #Preview {
-    MapView()
+    MapView( locations: .constant( [
+        Location(name: "Bob", coordinate: CLLocationCoordinate2DMake(36.0, -77)),
+        Location(name: "Bob", coordinate: CLLocationCoordinate2DMake(37.0, -78)),
+    ] ) )
 }
